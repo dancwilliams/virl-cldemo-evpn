1a982b39428acdee4fe7a4b6a0f6c4f9f736995d
dancwilliams <dan.c.williams@gmail.com>
2017-01-12 17:20:21 -0600

    fixed typo

--- config/leaf01/leaf_1_config.sh
+++ config/leaf01/leaf_1_config.sh
@@ -1,4 +1,4 @@
-#!/bin/b
+#!/bin/bash
 #This file is transferred to the Cumulus VX and executed to re-map interfaces
 #Extra config COULD be added here but I would recommend against that to keep this file standard.
 exec &> /var/log/virllogfile.txt
